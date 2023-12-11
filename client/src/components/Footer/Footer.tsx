14e8edbcf259ccccace0b7519833ead0800185fd
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-12-11 11:23:56 +0100

    minor styling fixes for footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import styles from "./Footer.module.scss";
 import useApi, { CONTACT_ENDPOINT } from "../../hooks/useApi";
 import { IContact } from "../../models/IContact";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   const { theme } = useTheme();
@@ -26,12 +27,7 @@ export const Footer = () => {
         color: theme.secondaryColor,
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
+      <Box className={styles.footerWrapper__contacts}>
         <Typography>{data?.data.attributes.website}</Typography>
         <Typography>{data?.data.attributes.company}</Typography>
         <Typography>{data?.data.attributes.phonenumber}</Typography>
@@ -39,13 +35,31 @@ export const Footer = () => {
       <Box>
         <Typography>{data?.data.attributes.email}</Typography>
       </Box>
-      <Box>
+      <Box className={styles.footerWrapper__freeText}>
         <Typography>{data?.data.attributes.freeText}</Typography>
       </Box>
-      <Box>
-        <FacebookIcon sx={{ color: theme.secondaryColor, fontSize: "2rem" }} />
-        <InstagramIcon sx={{ color: theme.secondaryColor, fontSize: "2rem" }} />
-        <YouTubeIcon sx={{ color: theme.secondaryColor, fontSize: "2rem" }} />
+      <Box className={styles.footerWrapper__socials}>
+        <Link
+          to={"https://www.facebook.com/xtools.se"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon
+            sx={{ color: theme.secondaryColor, fontSize: "2rem" }}
+          />
+        </Link>
+        <Link
+          to={"https://www.instagram.com/xtools.se/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon
+            sx={{ color: theme.secondaryColor, fontSize: "2rem" }}
+          />
+        </Link>
+        <Link to={"#"} target="_blank" rel="noopener noreferrer">
+          <YouTubeIcon sx={{ color: theme.secondaryColor, fontSize: "2rem" }} />
+        </Link>
       </Box>
     </Box>
   );
