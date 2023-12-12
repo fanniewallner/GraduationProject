80d4b29533e1eb93e2da7828b1eb23205217ac55
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-12-12 14:33:57 +0100

    implement cypress

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from "cypress";
-import cucumber from "cypress-cucumber-preprocessor";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { devServer } from "@cypress/vite-dev-server";
+import react from "@vitejs/plugin-react-swc";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import { createRollupPlugin } from "@badeball/cypress-cucumber-preprocessor/rollup";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import cypressCucumberPkg from "@badeball/cypress-cucumber-preprocessor";
 
+const { addCucumberPreprocessorPlugin } = cypressCucumberPkg;
 export default defineConfig({
+  video: false,
   e2e: {
     baseUrl: "http://localhost:5173",
-    specPattern: "./cypress/e2e/**/**.{feature,ts}",
+    specPattern: "./cypress/e2e/**/*.feature",
     supportFile: "cypress/support/e2e.ts",
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
     retries: {
       runMode: 3,
--- client/cypress/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import cucumber from "cypress-cucumber-preprocessor";
-
-export default (
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-) => {
-  on("file:preprocessor", cucumber());
-  return config;
-};
