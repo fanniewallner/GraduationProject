a4f0884076aa654bd448387d33124ea840e02f25
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-12-17 10:53:32 +0100

    basic implementation of error page

---
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,43 @@
+import { Container, Box, Typography, Button, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "../../contexts/ThemeContext";
+import styles from "./ErrorPage.module.scss";
+
+export const ErrorPage = () => {
+  const navigate = useNavigate();
+  const { theme } = useTheme();
+  return (
+    <Container
+      sx={{
+        height: "100vh",
+        pt: "5rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1rem",
+      }}
+    >
+      {/* <img src="/5203299.jpg" alt="error" /> */}
+      <Typography variant="h1" color={theme.secondaryColor}>
+        404
+      </Typography>
+      <Typography color={theme.secondaryColor}>
+        Sidan kunde tyvärr inte hittas..
+      </Typography>
+      <Typography color={theme.secondaryColor}>
+        Vi arbetar hårt för att få tillbaka den, men under tiden kan du kika
+        vidare på {""}
+        <Link
+          style={{ color: theme.secondaryColor, textDecoration: "underline" }}
+          component={Link}
+          href="/produktkatalog"
+        >
+          produktkatalogen.
+        </Link>
+      </Typography>
+      <Typography color={theme.secondaryColor}>
+        Vill du ha kontakt med oss? Mejla oss på kreativasnickare@gmail.com
+      </Typography>
+    </Container>
+  );
+};
