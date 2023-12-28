69f7f827ed31ca6124b407001f091245ff399e75
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-12-28 16:17:00 +0100

    added padding to purchase conditions container

--- client/src/pages/PurchaseConditions/PurchaseConditions.tsx
+++ client/src/pages/PurchaseConditions/PurchaseConditions.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import useApi from "../../hooks/useApi";
 import { Container, Typography } from "@mui/material";
+import { useTheme } from "../../contexts/ThemeContext";
 
 export const PurchaseConditions = () => {
   const api = useApi();
+  const theme = useTheme();
   const [loading, setLoading] = useState(true);
   const [contact, setContact] = useState<string>();
   useEffect(() => {
@@ -23,8 +25,12 @@ export const PurchaseConditions = () => {
   }, []);
   console.log(contact);
   return (
-    <Container sx={{ minHeight: "100vh", pt: "5rem" }}>
-      <Typography sx={{ whiteSpace: "pre-line" }}>{contact}</Typography>
+    <Container sx={{ minHeight: "100vh", pt: "5rem", pb: "2rem" }}>
+      <Typography
+        sx={{ whiteSpace: "pre-line", color: theme.theme.secondaryColor }}
+      >
+        {contact}
+      </Typography>
     </Container>
   );
 };
