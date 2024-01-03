0d377f5358926111667e6917df0c7bddd092c156
Fannie <113436254+fanniewallner@users.noreply.github.com>
2024-01-03 11:04:08 +0100

    added efont poppins to theme

--- client/src/contexts/ThemeContext.tsx
+++ client/src/contexts/ThemeContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
 
 const colors = {
-  primayBackgroundColor: "#1d1d1def",
+  primaryBackgroundColor: "#1d1d1def",
   secondaryBackgroundColor: "#556c56",
   primaryColor: "#556c56",
   secondaryColor: "#F7F4F4",
@@ -13,6 +13,9 @@ interface Theme {
     sm: number;
     md: number;
     lg: number;
+  };
+  typography: {
+    fontFamily: string;
   };
   primaryBackgroundColor: string;
   secondaryBackgroundColor: string;
@@ -27,7 +30,6 @@ interface ThemeContextProps {
 
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
@@ -47,7 +49,8 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
       md: 900,
       lg: 1280,
     },
-    primaryBackgroundColor: colors.primayBackgroundColor,
+    typography: { fontFamily: ["Poppins", "sans-serif"].join(",") },
+    primaryBackgroundColor: colors.primaryBackgroundColor,
     secondaryBackgroundColor: colors.secondaryBackgroundColor,
     primaryColor: colors.primaryColor,
     contrastColor: colors.contrastColor,
@@ -68,7 +71,9 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
           --primary-color: ${theme.primaryColor};
           --secondary-color: ${theme.secondaryColor};
           --contrast-color: ${theme.contrastColor};
+          --font-family: ${theme.typography.fontFamily};
         }
+
       `}</style>
     </ThemeContext.Provider>
   );
